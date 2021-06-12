1c951572cce1ae37f1898a46c1dcedcc3a53c2d3
KuecuekM <mehmetkucuk74@gmail.com>
2021-06-12 20:32:00 +0200

    Doing more work after initially publishing to gitHub

--- src/test/java/Practice1.java
+++ src/test/java/Practice1.java
@@ -2,5 +2,8 @@ public class Practice1 {
     public static void main(String[] args) {
         System.out.println("EU5 is liking Git on Intellij");
         System.out.println("WORK 1");
+
+        // successfully push project to GitHUb
+        // first Intellij automatically created  remote re
     }
 }
